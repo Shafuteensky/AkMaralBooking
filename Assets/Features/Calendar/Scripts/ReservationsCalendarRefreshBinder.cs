d3e06af649d3fa05b620147c4ce48ff88cb5af48
Ildar <ildaravgn@gmail.com>
2026-05-05 20:59:33 +0600

    Внешний вид календаря и обновление

---
+++ Assets/Features/Calendar/Scripts/ReservationsCalendarRefreshBinder.cs
@@ -0,0 +1,62 @@
+using Extensions.Data.InMemoryData.SelectionContext;
+using Extensions.ScriptableValues;
+using StarletBooking.Data;
+using UnityEngine;
+
+namespace StarletBooking.Calendar
+{
+    /// <summary>
+    /// Обновляет календарь записей аренды при изменении фильтров
+    /// </summary>
+    [RequireComponent(typeof(ReservationsMonthViewController))]
+    public sealed class ReservationsCalendarRefreshBinder : MonoBehaviour
+    {
+        [SerializeField] private BaseSelectionContext houseSelectionContext;
+        [SerializeField] private ReservationsDataContainer reservationsDataContainer;
+        [SerializeField] private DateValue arrivalDateFilter;
+        [SerializeField] private DateValue departureDateFilter;
+
+        private ReservationsMonthViewController calendar;
+
+        private void Awake()
+        {
+            calendar = GetComponent<ReservationsMonthViewController>();
+        }
+
+        private void OnEnable()
+        {
+            if (houseSelectionContext is HouseSelectionContext houseContext)
+                houseContext.onSelectionChanged += Refresh;
+
+            if (arrivalDateFilter != null)
+                arrivalDateFilter.onValueChanged += OnDateValueChanged;
+
+            if (departureDateFilter != null)
+                departureDateFilter.onValueChanged += OnDateValueChanged;
+            
+            if (reservationsDataContainer != null)
+                reservationsDataContainer.onDataUpdated += Refresh;
+            
+            Refresh();
+        }
+
+        private void OnDisable()
+        {
+            if (houseSelectionContext is HouseSelectionContext houseContext)
+                houseContext.onSelectionChanged -= Refresh;
+
+            if (arrivalDateFilter != null)
+                arrivalDateFilter.onValueChanged -= OnDateValueChanged;
+
+            if (departureDateFilter != null)
+                departureDateFilter.onValueChanged -= OnDateValueChanged;
+            
+            if (reservationsDataContainer != null)
+                reservationsDataContainer.onDataUpdated -= Refresh;
+        }
+
+        private void OnDateValueChanged(string value) => Refresh();
+
+        private void Refresh() => calendar.RefreshReservationsCalendar();
+    }
+}
